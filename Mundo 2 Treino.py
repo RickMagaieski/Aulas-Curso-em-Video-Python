5c28c8da60885816e0c93e4f686a4e937f835619
RickMagaieski <henrimagaieski@icloud.com>
2023-07-18 13:23:35 -0300

    Treinos do mundo 2

--- Mundo 2 Treino.py
+++ Mundo 2 Treino.py
@@ -110,5 +110,51 @@ else:
     print(f"\033[1mPortanto o número\033[m\033[1;31m {num}\033[m\033[1m não é PRIMO...\033[m")"""
 
 
+
 #Treino de FOR: Ex053  - Detector de Palíndromos
 
+"""from time import sleep
+
+frase = input("Digite algo: ").upper().strip()
+separar = frase.split()
+juntar = ''.join(separar)
+inverso = ''
+for p in range(len(juntar)-1, -1, -1):
+    inverso += juntar[p]
+print(f"A frase {frase} ao contrário fica {inverso}...")
+sleep(1.5)
+if juntar == inverso:
+    print("Portanto, a sua frase é um PALINDROMO!")
+else:
+    print("Portanto, a sua frase não é um PALINDROMO...")"""
+
+
+
+#Treino de FOR: Ex054 -
+
+"""from datetime import date
+
+perg = int(input("Você quer colocar quantas datas? "))
+hoje = date.today().year
+cont1 = 0
+cont2 = 0
+for m in range(1, perg + 1):
+    data = int(input(f"Qual é a data de nascimento da {m}° pessoa? "))
+    conta = hoje - data
+    if conta < 18:
+        cont1 += 1
+    elif conta >= 18:
+        cont2 += 1
+print(f"Das {perg} datas de nascimentos inseridas, {cont2} são de pessoas maiores de 18 anos e {cont1} são de menores.")
+"""
+
+
+
+#Treino de FOR: Ex055 - Maior e menor da sequência
+
+perg = int(input("Digite quantos pesos serão lidos: "))
+lista = []
+for p in range(1, perg + 1):
+    peso = float(input(f"Digite o peso da {p}° pessoa: "))
+    lista.append(peso)
+print(f"O maior peso foi {max(lista)}Kg e o menor foi {min(lista)}Kg")
