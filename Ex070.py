4f089fb59b3e556f65efd011462e182c0f17c1bf
RickMagaieski <henrimagaieski@icloud.com>
2023-08-10 11:14:55 -0300

    Exercicio 70 terminado

--- Ex070.py
+++ Ex070.py
@@ -7,10 +7,12 @@ print(" " * 10, "MAGAIESKI'S SHOP")
 print("-=" * 20)
 print(void)
 
-produtos = ''
-valorpq = 0
+produtos = []
+produto2 = ''
+menorv = []
 cont1000 = 0
-soma = 0
+soma = 0.0
+cont = 0
 
 
 while True:
@@ -21,16 +23,24 @@ while True:
     print(void)
 
     soma += valor
-    valorpq.append(valor)
+    menorv.append(valor)
+    produtos.append(produto)
+    minimo = min(menorv)
+    cont += 1
 
-    if valorpq < 9999:
-        produtos = produto
+    if produto and valor == minimo:
+        produto2 = produto
     if valor > 1000:
         cont1000 += 1
     if perg == "N":
         print(void)
-        print("-=" * 10, "FIM DO PROGRAMA", "-=" * 10)
-        print("O total da compra foi R${:.2f}".format(soma))
+        print("-=" * 10, "FIM DO PROGRAMA", "-=" * 10,"-")
+        print(f"Foram comprados {cont} itens ao todo, os quais serão listados abaixo:")
+        for item in produtos:
+            print(f".{item}")
+            sleep(1)
+        print(f"O total da compra foi R${soma:.2f}")
         print(f"Temos {cont1000} produto(s) custando mais de R$1000.00")
-        print(f"O produto mais barato foi {produtos} que custa R${menor}")
+        print(f"O produto mais barato foi o(a) {produto2} que custa R${minimo:.2f}")
+
         break
