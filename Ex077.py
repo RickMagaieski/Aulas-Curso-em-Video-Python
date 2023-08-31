d9330042572bfaeede3863ba9fb514be713b8a01
RickMagaieski <henrimagaieski@icloud.com>
2023-08-31 14:48:32 -0300

    Essa parada fica mais confusa a cada dia haahhahaah

--- Ex077.py
+++ Ex077.py
@@ -1,3 +1,7 @@
-palavras = ('arvore', 'terra')
+palavras = ('ARVORE', 'TERRA', 'GRAMA', 'AREIA', 'PEDRA', 'FRUTA', 'FOLHA', 'GIRASSOL', 'CEU', 'TRONCO', 'FLOR'
+            , 'NUVEM', 'AR', 'FOGO', 'GELO')
 for p in palavras:
-    print(f"Para a palavra {p}, temos: ")
+    print(f"\nPara a palavra {p}, temos: ", end='')
+    for letra in p:
+        if letra in 'AEIOU':
+            print(letra, end=' ')
