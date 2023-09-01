f1ac70da10eb266a2f77e372d277d61709f259de
RickMagaieski <henrimagaieski@icloud.com>
2023-09-01 09:53:19 -0300

    Correção e treino

--- Ex072.py
+++ Ex072.py
@@ -4,10 +4,10 @@ numeros = ('Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight
 void = ''
 
 while True:
-    di = int(input('Type a number between 0 and 20: '))
+    di = int(input('Digite um número entre 0 e 20: '))
     print(void)
     if di < 0 or di > 21:
-        print("Error, unknown command, try again!")
+        print("Erro, comando desconhecido, tente novamente!")
     else:
-        print(f"You typed number \033[1m{numeros[di]}\033[m")
+        print(f"Você digitou o número \033[1m{numeros[di]}\033[m")
         break
---
+++ Mundo 3 Treino.py
@@ -0,0 +1,21 @@
+#Treino de TUPLAS: Ex072 - Número por extenso
+
+"""numeros = ("UM", "DOIS", "TRES", "QUATRO", "CINCO",
+    "SEIS", "SETE", "OITO", "NOVE", "DEZ",
+    "ONZE", "DOZE", "TREZE", "QUATORZE", "QUINZE",
+    "DEZESSEIS", "DEZESSETE", "DEZOITO", "DEZENOVE", "VINTE")
+void = ''
+
+while True:
+    perg = int(input("Digite um número entre 0 e 20: "))
+    if perg > 20 or perg < 0:
+        print("Erro, comando desconhecido, tente novamente!")
+        print(void)
+    else:
+        print("O número digitado foi o numero", numeros[perg - 1])
+        break"""
+
+
+
+
+#Treino de TUPLAS: Ex073 -
