6db70f543a3d91b63fa06d6f9dbbaf961951205d
RickMagaieski <henrimagaieski@icloud.com>
2023-09-11 12:01:41 -0300

    Exercicio 078 (incompleto)

--- Ex078.py
+++ Ex078.py
@@ -1,13 +1,27 @@
 lista = []
+lista2 = []
+lista3 = []
 void = ''
 
 for num in range(0, 5):
     perg = int(input(f"Digite um número na posição {num}: "))
     lista.append(perg)
+maxv = max(lista)
+minv = min(lista)
+
+for i, valor in enumerate(lista):
+    if valor == maxv:
+        lista2.append(i)
+    if valor == minv:
+        lista3.append(i)
+
 print(f"Você digitou os seguintes valores: {lista}")
 print(void)
-print(f"O maior valor desta lista é o valor {max(lista)}, encontrado nas seguintes posições: ")
-print(f"O menor valor desta lista é o valor {min(lista)}, encontrado nas sequintes posições: ")
-print(void)
-print(lista.index(max(lista)))
-print(lista.index(min(lista)))
+
+if lista2 > [0]:
+    print(f"O maior valor desta lista é o valor {maxv}, encontrado nas seguintes posições: {lista2}")
+if lista3 > [0]:
+    print(f"O menor valor desta lista é o valor {minv}, encontrado nas sequintes posições: {lista3}")
+else:
+    print(f"O maior valor desta lista é o valor {maxv}, encontrado na posição {lista.index(maxv)}")
+    print(f"O menor valor desta lista é o valor {minv}, encontrado na posição {lista.index(minv)}")
