a84d3d6596083dff1105069f6338faf3bdaf1bf3
RickMagaieski <henrimagaieski@icloud.com>
2023-09-12 10:53:14 -0300

    Exercice 079

---
+++ Ex079.py
@@ -0,0 +1,21 @@
+listy = []
+ques2 = ''
+void = ''
+
+while True:
+    ques = int(input("Type a number: "))
+    if ques not in listy:
+        listy.append(ques)
+    else:
+        print("Value already typed! Type another one.")
+    ques2 = str(input("You want continue? [Y/N] ")).upper()
+    if ques2 == 'N':
+        break
+    if ques2 != 'Y':
+        print("Invalid Command")
+        break
+print(void)
+if ques2 == 'N':
+    print(f"You've added these values: {listy}")
+if ques2 != 'N' or 'Y':
+    print("Program Finished")
