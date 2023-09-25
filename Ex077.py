e1a61b18679b23670bab03d2eed905665b1d2713
RickMagaieski <henrimagaieski@icloud.com>
2023-09-25 16:19:09 -0300

    Exclusão

--- Ex077.py
+++
@@ -1,7 +0,0 @@
-palavras = ('ARVORE', 'TERRA', 'GRAMA', 'AREIA', 'PEDRA', 'FRUTA', 'FOLHA', 'GIRASSOL', 'CEU', 'TRONCO', 'FLOR'
-            , 'NUVEM', 'AR', 'FOGO', 'GELO')
-for p in palavras:
-    print(f"\nPara a palavra {p}, temos: ", end='')
-    for letra in p:
-        if letra in 'AEIOU':
-            print(letra, end=' ')
