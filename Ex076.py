25766382187d5912b4fe5a619f097fa5d015a487
RickMagaieski <henrimagaieski@icloud.com>
2023-09-27 15:01:44 -0300

    Exercício 76

--- Ex076.py
+++ Ex076.py
@@ -1,15 +1,23 @@
+void = ''
+
 print(f"{'='*50}")
 print(f"{'THE MAGAIESKI SHOP':>34}")
 print(f"{'='*50}")
 
-produtos = ('Papel', 20,
-            'Caneta', 30,
-            'Lápis', 40,
-            'Borracha', 50,
-            'Caderno', 60,
-            'Mochila', 70)
-for lis in produtos:
-    if len(produtos) % 2 == 0:
-        print(f"{lis}", end=' ')
-    else:
-        print(f"\n{lis}")
+print(void)
+
+produtos = ('Bala', 0.50,
+            'Chocolate', 12.09,
+            'Caixa de Bombom', 15.34,
+            'Tic Tac', 3.50,
+            'Bolacha', 5.47,
+            'Iogurte', 5.30,
+            'Sorvete', 4.94,
+            'Geladinho', 2.50,
+            'Biscoito', 6.89,
+            'Pirulito', 1.00)
+
+for lis in range(0, len(produtos), 2):
+    print(f"{produtos[lis]:.<42}{'R$'}", end=' ')
+    print(f"{produtos[lis + 1]:>5.2f}")
+print("=" * 50)
