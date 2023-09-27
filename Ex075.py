812c1e754f0820f7e9614514f35ff2ba3342e2d8
RickMagaieski <henrimagaieski@icloud.com>
2023-09-27 09:21:36 -0300

    Exercício 075 (Ajustes)

--- Ex075.py
+++ Ex075.py
@@ -4,7 +4,7 @@ void = ''
 cont = 0
 
 for n in range(0, 4):
-    perg = int(input(f"Digite o numero para posição {n}º: "))
+    perg = int(input(f"Digite o {n+1}º numero: "))
     numeros.append(perg)
     if perg == 9:
         cont += 1
@@ -17,14 +17,17 @@ print(void)
 
 print(f"Os números digitados foram os seguintes: {tupla}")
 print(void)
-print(f"O valor 9 apareceu {cont} vez(es)")
+if 9 in tupla:
+    print(f"O valor 9 apareceu {cont} vez(es)")
+else:
+    print(f"O valor 9 não foi digitado.")
 
 if 3 in tupla:
     print(f"O número 3 apareceu na posição {tupla.index(3)}")
 else:
-    print("O valor 3 não foi encontrado em nenhuma posição")
+    print("O valor 3 não foi encontrado em nenhuma posição.")
 
-print("Os valores pares foram:", end=' ')
+print("Os valores pares foram:", end='\033[1m | \033[m')
 
 for par in tupla2:
-    print(par, end=' ')
+    print(par, end='\033[1m | \033[m')
