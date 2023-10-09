1a9fed80cbc493592ca21d3c2bb5079e3bcee908
RickMagaieski <henrimagaieski@icloud.com>
2023-10-09 18:06:51 -0300

    Reforço

--- Aula 017.py
+++ Aula 017.py
@@ -1,4 +1,4 @@
-"""lista = [10, 12, 13, 43, 50, 21, 453, 102, 3]
+lista = [10, 12, 13, 43, 50, 21, 453, 102, 3]
 print(f"O maior número da lista é o número {max(listan)}")
 print(f"O menor número da lista é o número {min(listan)}")
 listan.pop(3)
@@ -32,7 +32,7 @@ for r in range(1, 7):
     lista.append(randint(1, 10))
     for re in lista:
         if 3 in lista:
-            lista.remove(3)
+            lista.remove(3) #Somente se tiver o 3 ele irá excluir
 print(lista)
 
 # Esse é um programa onde números aleatórios são gerados, nos quais qualquer número que aparecer 3 será eliminado
@@ -68,4 +68,35 @@ print(f"Essa é a lista B: {b}")
 
 # Quando você faz isso, você não está criando uma cópia, você está interligando ambas as listas!
 # Já para criar uma cópia você precisa fazer isso: b = a[:]. Assim, todos os números de A estarão em B, ou uma cópia :)
-"""
+
+
+
+num = [2, 5, 9, 1]
+num[2] = 3   #Adiciona o numero 3 na posição 3
+num.append(7)
+num.sort(reverse=True)   #Inverte a lista
+num.insert(2, 2)
+
+#num.pop(2)   #Exclui o número na posição 2
+
+if 4 in num:
+    num.remove(4)
+else:
+    print("Não achei o número 4")
+
+print(num)
+print(f"Essa lista tem {len(num)} elementos")
+
+
+
+valores = []
+valores.append(5)
+valores.append(9)
+valores.append(4)
+
+for cont in range(0, 5):
+    valores.append(int(input("Digite um valor: ")))
+
+for c, v in enumerate(valores):
+    print(f"Na posição {c} encontrei o valor {v}")
+print("Cheguei ao final da lista")"""
