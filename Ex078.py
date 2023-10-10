39de359465734b228fa620f943410e4337846842
RickMagaieski <henrimagaieski@icloud.com>
2023-10-10 08:40:02 -0300

    Delete Ex078.py

--- Ex078.py
+++
@@ -1,7 +0,0 @@
-v = []
-
-for p in range(0, 5):
-    v.append(int(input(f"Digite um número para a posição {p}: ")))
-
-print(f"Você digitou os seguintes valores: {v}")
-print(f"O maior número foi o {max(v)} encontrado nas posições: {v.index(max(v))}...")
