963e182ff3b31e063a651f56b86ee4f32b36c479
RickMagaieski <henrimagaieski@icloud.com>
2023-10-31 16:19:37 -0300

    Exercicio 084

--- Ex084.py
+++ Ex084.py
@@ -1,31 +1,43 @@
 lista = []
 galera = []
 void = ''
-cont = 0
-mai = []
-men = []
+mai = men = 0
 
 while True:
     lista.append(str(input("Nome: ")))
     lista.append(float(input("Peso: ")))
-    cont += 1
-    galera.append(lista[:])
 
     print(void)
 
-    perg = str(input("Quer continuar? [S/N] ")).upper()[0]
+    if len(galera) == 0:
+        men = mai = lista[1]
+    else:
+        if lista[1] > mai:
+            mai = lista[1]
+        if lista[1] < men:
+            men = lista[1]
+
+    galera.append(lista[:])
+    lista.clear()
+
+    perg = str(input("Você quer continuar? [S/N] ")).upper()[0]
 
     print(void)
 
     if perg != 'S' and perg != 'N':
-        print("Comando Desconhecido, tente novamente.")
+        print("Comando Inválido, tente novamente...")
+        break
+    if perg in 'N':
         break
 
-    if perg == 'N':
-        break
 
-print("=" * 46)
-
-print(f"Ao todo, você cadastrou {cont} pessoas.")
-print(f"O maior peso foi Kg das seguintes pessoas: ")
-print(f"O menor peso foi Kg das seguintes pessoas: ")
+print("-=" * 30)
+print(f"A quantidade de pessoas cadastradas foi {len(galera)}")
+print(f"O maior peso foi {mai}Kg das seguintes pessoas: ", end=' ')
+for p in galera:
+    if p[1] == mai:
+        print(f"[{p[0]}]", end=' ')
+print(f"\nO menor peso foi {men}Kg das seguintes pessoas: ", end=' ')
+for p in galera:
+    if p[1] == men:
+        print(f"[{p[0]}]", end=' ')
