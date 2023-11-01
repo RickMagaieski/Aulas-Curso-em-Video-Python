2d93d9168aae9403b39ed22cbebe949eff926d05
RickMagaieski <henrimagaieski@icloud.com>
2023-11-01 14:24:30 -0300

    Ex027

---
+++ Ex027.py
@@ -0,0 +1,10 @@
+nome = str(input("Digite seu nome completo: "))
+separado = nome.split()
+void = ''
+
+print(void)
+
+print("Prazer em te conhecer!")
+print(void)
+print(f"Seu primeiro nome é {separado[0]}")
+print(f"Seu último nome é {separado[-1]}")
