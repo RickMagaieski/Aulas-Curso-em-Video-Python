fe1a0f7b07abebc099b601a2e8ff2ba698fef2aa
RickMagaieski <henrimagaieski@icloud.com>
2023-11-01 14:21:24 -0300

    Ex026

---
+++ Ex026.py
@@ -0,0 +1,7 @@
+f = str(input("Digite algo: ")).upper().strip()
+void = ''
+
+print(void)
+print(f"A letra A aparece {f.count('A')} vez(es).")
+print(f"A primeira letra A aparece na posição {f.find('A')}")
+print(f"A última letra A aparece na posição {f.rfind('A')}")
