8ab75e6b7598395d8e65749983e7f758e022f00e
RickMagaieski <henrimagaieski@icloud.com>
2023-11-02 15:46:11 -0300

    Ex030

---
+++ Ex030.py
@@ -0,0 +1,8 @@
+print()
+
+n = int(input("\033[1mDigite um número:\033[m "))
+
+if n % 2 == 0:
+    print("\033[1mEsse número é um número par!\033[m")
+else:
+    print("\033[1mEsse número é um número ímpar!\033[m")
