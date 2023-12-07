2386985864f1124a7589b808474c48b64c248b54
Henry Magaieski <136091273+RickMagaieski@users.noreply.github.com>
2023-12-07 15:30:01 -0500

    Ex038

---
+++ Ex038.py
@@ -0,0 +1,13 @@
+print()
+
+p1 = int(input("Primeiro Valor: "))
+p2 = int(input("Segundo Valor: "))
+
+print()
+
+if p1 > p2:
+    print("O primeiro valor e o maior!")
+elif p2 > p1:
+    print("O segundo valor e o maior!")
+else:
+    print("Ambos sao iguais.")
