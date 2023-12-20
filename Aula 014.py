516d78c999b176c3e4074c964c1c7763c0acd088
Henry Magaieski <136091273+RickMagaieski@users.noreply.github.com>
2023-12-20 12:28:26 -0500

    Aula 014

---
+++ Aula 014.py
@@ -0,0 +1,20 @@
+"""
+n = 1
+par = impar = 0
+
+while n != 0:
+    n = int(input("Digite um valor: "))
+    if n != 0:
+        if n % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+print(f"Voce digitou {par} numero(s) par(es) e {impar} numero(s) impar(es).")"""
+
+c = 1
+
+while c != 11:
+    print(c, end=" ")
+    c += 1
+
+print("Acabou")
